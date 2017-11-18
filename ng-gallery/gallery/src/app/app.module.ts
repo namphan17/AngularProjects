37413082c9c1cfe2e48838f878c09d7acdc1d9ea
tibuchuba <acsaxsax95@gmail.com>
2017-11-17 23:06:10 -0500

    bootstrp-installation

---
+++ ng-gallery/gallery/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+import { AppComponent } from './app.component';
+import  {NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AlertModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
